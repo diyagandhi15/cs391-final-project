806808a1c6588bed5c147ec2c1bade1fe62f9d8c
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-12-05 16:24:43 -0500

    Added code for playlist

--- app/playlists/playlist-list.tsx
+++ app/playlists/playlist-list.tsx
@@ -11,6 +11,26 @@ export default function PlaylistList() {
   useEffect(() => {
     const fetcher = async () => {
       try {
+        // Refresh the access token
+        const body = await fetch("https://accounts.spotify.com/api/token", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: new URLSearchParams({
+            grant_type: "refresh_token",
+            refresh_token: localStorage.getItem("refresh_token") as string,
+            client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID as string,
+          }),
+        });
+        const response = await body.json();
+
+        localStorage.setItem("access_token", response.accessToken);
+        if (response.refreshToken) {
+          localStorage.setItem("refresh_token", response.refreshToken);
+        }
+
+        // fetch user playlist data
         const res = await fetch("https://api.spotify.com/v1/me/playlists", {
           method: "GET",
           headers: {
