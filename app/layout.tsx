341021e9314b5a14b2b044e2f3f3d94eac21ee6b
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-12-05 01:43:07 -0500

    Added metadata for our app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 // Author: Diya Gandhi
 // This component defines the root layout of the application, providing global structure, styles, and the UserContext for state management.
 
-"use client";
+// "use client";
 import { UserProvider } from "@/contexts/UserContext";
 import { ReactNode } from "react";
 import NavBar from "./navbar";
+
+export const metadata = {
+  title: "Spotify Insights App",
+  description: "App to view your spotify insights",
+  icons: { icon: "/favicon.ico" },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
